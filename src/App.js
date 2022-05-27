081588f1d69139003b3e70caf6eb908fa9b60b3b
akifzsche <akif.tonmaister58@gmail.com>
2022-05-27 12:09:43 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import Content from './Components/Content';
-import './App.css';
-import { ThemeProvider } from './Components/ThemeContext'
+import Content from "./Components/Content";
+import "./App.css";
+import { ThemeProvider } from "./Components/ThemeContext";
 function App() {
-
-
-
-
   return (
     <div>
       <ThemeProvider>
-      <Content/>
+        <Content />
       </ThemeProvider>
-      
     </div>
   );
 }
